fbfa8022fad3bbfa29ba403c070adaec8a18cb6e
amal631 <82928108+amal631@users.noreply.github.com>
2026-01-09 10:48:32 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function motorOn() {
+    alert("Motor ON button clicked");
+}
+
+function motorOff() {
+    alert("Motor OFF button clicked");
+}
+
+function increaseLoad() {
+    alert("Load Increase clicked");
+}
+
+function decreaseLoad() {
+    alert("Load Decrease clicked");
+}
